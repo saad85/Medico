cef4bcab9305c050e9a92e5354189e6d2f73e1e2
saad85 <samiul.saad@gmail.com>
2020-09-07 18:57:39 -0700

    mongo db added. user addded. #001

--- components/tools/Modal/modal.js
+++ components/tools/Modal/modal.js
@@ -1,10 +1,36 @@
 import { Button ,Modal,Popover} from 'react-bootstrap';
 import LoginForm from '../../Login-SignUp-Form/login'
+import SignUp from '../../Login-SignUp-Form/signUp'
+import Login from '../../Login-SignUp-Form/signUp';
+
+function ModalTitle(props){
+
+  let title = 'Log in'; 
+  if(props.modalType === 'signUp') title = 'Sign Up'; 
+
+  return <Modal.Title><a>{title}</a></Modal.Title>
+}
+
+function ModalBody(props){
+
+  console.log("modalType ",props)
+
+  let bodyComponent = <LoginForm />;
+
+  if(props.modalType === 'signUp') bodyComponent = <SignUp/>
+
+  console.log("bodyComponent ",bodyComponent)
+                
+  return <Modal.Body>{bodyComponent} </Modal.Body>
+}
+
+
 
 class CustomModal extends React.Component{
     //data
     state={
-        show:false
+        show:false,
+        modalType:'logIn'
     }
 
     //hooks
@@ -15,6 +41,11 @@ class CustomModal extends React.Component{
     }
 
     //methods
+    changeModalTypeFromChild =(props) =>{
+      console.log(props);
+
+      this.setState({modalType:'signUp'});
+    }
     handleClose=()=>{
         this.setState({show:false});
         this.props.sendDataToParent({isShowLoginModal:false});
@@ -26,23 +57,25 @@ class CustomModal extends React.Component{
         
               <Modal show={this.state.show} onHide={this.handleClose} size="sm" >
                 
-                <Modal.Header closeButton>
-                  <Modal.Title><a>Log in</a></Modal.Title>
+                <Modal.Header closeButton className="custom-modal-header">
+                    <ModalTitle modalType = {this.state.modalType}/>
                 </Modal.Header>
-                
-                <Modal.Body>
-                    <LoginForm/>
-                </Modal.Body>
-                
-                <Modal.Footer>
 
+              <Modal.Body> 
+                {this.state.modalType === 'signUp' ? <SignUp/> : <LoginForm onChangeModalType={this.changeModalTypeFromChild}/>}
+              </Modal.Body>
                 
-                </Modal.Footer>
+              
+                <Modal.Footer></Modal.Footer>
               </Modal>
 
               <style jsx>{`
                   .modal-30w{
                         width:50%!important
+                }
+                .custom-modal-header{
+                  background-color: #127ba3!important;
+                  color:#fff!important;
                 }
 
         `}</style>
